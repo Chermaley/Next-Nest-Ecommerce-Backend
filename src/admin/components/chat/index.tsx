3efe2a4bf879dda885ae03a64b07b33e277233d6
Chermaley <slimix23@gmail.com>
2022-08-29 15:34:38 +0700

    fix

--- src/admin/components/chat/index.tsx
+++ src/admin/components/chat/index.tsx
@@ -13,7 +13,7 @@ import ChatList from './components/ChatList';
 import { BasePropertyProps, useCurrentAdmin } from 'adminjs';
 import { Box } from '@adminjs/design-system';
 import { Message } from '../../../chat/models/message.model';
-import styled from 'styled-components';
+import * as styled from 'styled-components';
 
 export enum ChatEvent {
   CreateConsultation = 'createConsultation',
